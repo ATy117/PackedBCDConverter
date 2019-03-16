d4a687237e252d942e5f823fd31d81fbc76ad967
rebecabecalyn <45415030+rebecabecalyn@users.noreply.github.com>
2019-03-16 14:27:54 +0800

    View

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage){
+        View view = new View(primaryStage);
+    }
+
+    public static void main(String[] args){
+        launch(args);
     }
 }
---
+++ src/View.java
@@ -0,0 +1,55 @@
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class View {
+    @FXML public AnchorPane mainAnchorPane;
+    @FXML public JFXTextField inputTextField;
+    @FXML public JFXTextField expTextField;
+    @FXML public JFXTextField signField;
+    @FXML public JFXTextField combiField;
+    @FXML public JFXTextField ecbField;
+    @FXML public JFXTextField mcbFieldOne;
+    @FXML public JFXTextField mcbFieldTwo;
+
+
+    public Stage stage;
+    public View(Stage primaryStage){
+        this.stage = primaryStage;
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("viewTemplate.fxml"));
+        loader.setController(this);
+
+        Parent root = null;
+        try{
+            root= (Parent) loader.load();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+        init();
+
+        Scene scene= new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private void init(){
+
+        signField.setEditable(false);
+        combiField.setEditable(false);
+        mcbFieldOne.setEditable(false);
+        mcbFieldTwo.setEditable(false);
+        ecbField.setEditable(false);
+
+    }
+
+}
