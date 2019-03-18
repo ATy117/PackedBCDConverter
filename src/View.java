bc4d71e6f09c6ccd7b2928f463cc6bc89a24790e
rebecabecalyn <45415030+rebecabecalyn@users.noreply.github.com>
2019-03-18 18:16:07 +0800

    center

--- src/View.java
+++ src/View.java
@@ -2,6 +2,7 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -57,6 +58,9 @@ public class View {
         mcbFieldOne.setEditable(false);
         mcbFieldTwo.setEditable(false);
         ecbField.setEditable(false);
+
+        remarksLabel.setMaxWidth(Double.MAX_VALUE);
+        remarksLabel.setAlignment(Pos.CENTER);
 
     }
 
